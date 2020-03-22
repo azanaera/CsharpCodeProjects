2af0f15470cf9d7af190e4d28a64ce9f70dffed1
Era Azana <azanaera691@gmail.com>
2020-03-22 22:21:16 +0800

    Added Moq-xUnit

---
+++ Moq-XUnitTest/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Moq_XUnitTest;
+
+public class Driver
+{
+    private IVehicle vehicleToDrive;
+
+    public Driver(IVehicle vehicleToDrive)
+    {
+        this.vehicleToDrive = vehicleToDrive;
+    }
+
+    public bool EvasiveManeuvers(bool alertOffendingDriver)
+    {
+        bool success = false;
+        if (alertOffendingDriver)
+            success = this.vehicleToDrive.ApplyBrakes() && this.vehicleToDrive.HonkHorn();
+        else
+            success = this.vehicleToDrive.ApplyBrakes();
+
+        return success;
+    }
+}
---
+++ Moq-XUnitTest/IVehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moq_XUnitTest
+{
+    public interface IVehicle
+    {
+        ///<summary>
+        ///Honks the vehicle's horn.
+        ///</summary>
+        ///<returns>
+        ///True if the action was successful.
+        ///</returns>
+        bool HonkHorn();
+
+        ///<summary>
+        ///Applies the vehicle's brakes.
+        ///</summary>
+        ///<returns>
+        ///True if the action was successful.
+        ///</returns>
+        bool ApplyBrakes();
+    }
+}
